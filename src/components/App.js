014fade94a3b4e169063cef3c3ac65a338057fb2
bug41 <bugkingus@gmail.com>
2022-03-01 12:46:40 -0800

    pics 백업

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSearchSubmit(term){
-      console.log(term + "여기서 반응")
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+
+      const response = await unsplash.get('', {
+        params:{query : term},
+      });
+
+      this.setState({images: response.data.results})
     }
 
     render() {
         return <div className="ui container" style={{marginTop:'10px'}}>
             <SearchBar onSubmit={this.onSearchSubmit}/>
-        </div>
+            Found : {this.state.images.length} images    
+           </div>
     }
 }
 
